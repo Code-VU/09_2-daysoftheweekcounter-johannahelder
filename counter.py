5de025aabeb24bde755836367942fb84a51232a0
Hannah Elder <Hannah.Elder@vu.com>
2023-04-09 20:17:34 -0500

    v1

--- counter.py
+++ counter.py
@@ -1,6 +1,18 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
+    sender_day = []
+    days_dict= dict()
+
+    with open(file_name, 'r') as user_file:
+        local_file = user_file.readlines()
+
+    for line in local_file:
+        if line.startswith('From '):
+            sender_day.append(line.split(' ')[2])
+    for key in sender_day:
+        days_dict[key] = days_dict.get(key,0) +1
+    print(days_dict)
 
 
 
